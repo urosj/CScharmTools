ee8ffe6e2356037381e0992884c61b004b225079
Uros Jovanovic <uros.jovanovic@animaplus.net>
2015-06-18 07:14:18 +0200

    * daily stats file dump

---
+++ statsAggregator.py
@@ -0,0 +1,9 @@
+import sys
+import requests
+import json
+import random
+import datetime
+
+#server_url = "https://api.jujucharms.com/charmstore/v4"
+server_url = "http://10.0.1.234/charmstore/v4"
+
